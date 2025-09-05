ad840f257b89464b574c04930b8bc9ef734c7a32
keiseiTi <yuchacha250@gmail.com>
2025-09-05 16:59:19 +0800

    chore: add husky, commitlint

--- eslint.config.js
+++ eslint.config.js
@@ -2,9 +2,10 @@ import js from '@eslint/js';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 import pluginReact from 'eslint-plugin-react';
-import { defineConfig } from 'eslint/config';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
+  globalIgnores(['**/dist/', '**/types/']),
   {
     files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     plugins: { js },
