7105384b908149adda252981b6fc41ae36e04e27
keiseiTi <yuchacha250@gmail.com>
2026-01-09 15:40:46 +0800

    chore: add new site keyword

--- site/rspress.config.ts
+++ site/rspress.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   root: path.join(__dirname, 'docs'),
   base: '/tangramino',
   title: 'Tangramino',
-  description: '可视化低代码搭建',
+  description: 'Tangramino 可视化低代码搭建 LowCode 平台，支持多人协同开发。',
   icon: '/icon.svg',
   logoText: 'Tangramino',
   logo: {
@@ -14,7 +14,7 @@ export default defineConfig({
   },
   head: [
     '<meta name="google-site-verification" content="29-9koS6Iq6wX-a_UpdGx3b8CF6xsL3nXQXam3WZ2cE" />',
-    '<meta name="keyword" content="低代码,lowCode,多人协同" />',
+    '<meta name="keyword" content="Tangramino,lowCode,低代码,多人协同,可视化搭建" />',
   ],
   themeConfig: {
     outlineTitle: '大纲',
